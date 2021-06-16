c5a0fa730f26c4b95302ab4d0d18596448bf206a
Achyut Chapagain <71538964+ABChapagain@users.noreply.github.com>
2021-06-16 21:01:19 +0545

    Add files via upload

---
+++ app.js
@@ -0,0 +1,36 @@
+const inputs = document.querySelectorAll(".input");
+
+function focusFunc() {
+  let parent = this.parentNode;
+  parent.classList.add("focus");
+}
+
+function blurFunc() {
+  let parent = this.parentNode;
+  if (this.value == "") {
+    parent.classList.remove("focus");
+  }
+}
+
+inputs.forEach((input) => {
+  input.addEventListener("focus", focusFunc);
+  input.addEventListener("blur", blurFunc);
+});
+
+
+// Smooth scrolling
+
+$('.nav a, .cta').on('click', function(event) {
+  if (this.hash !== '') {
+    event.preventDefault();
+
+    const hash = this.hash;
+
+    $('html, body').animate(
+      {
+        scrollTop: $(hash).offset().top - 100
+      },
+      800
+    );
+  }
+});
